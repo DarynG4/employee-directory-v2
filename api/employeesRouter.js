dc31b59440d410bf7862eea974ef94fedf65ebcf
Daryn Gregory <daryn.greg@icloud.com>
2026-03-05 21:09:43 -0500

    completed!

---
+++ api/employeesRouter.js
@@ -0,0 +1,47 @@
+import express from "express";
+import employees from "#db/employees";
+
+const router = express.Router();
+
+export default router;
+
+router.get("/", (req, res) => {
+  res.send(employees);
+});
+
+router.post("/", (req, res) => {
+  if (!req.body) {
+    return res.status(400).send("Request body is required.");
+  }
+  const { name } = req.body;
+  if (!name) {
+    return res.status(400).send("Name is required.");
+  }
+  const newEmployee = {
+    id: employees.length + 1,
+    name,
+  };
+  employees.push(newEmployee);
+  res.status(201).send(newEmployee);
+});
+
+// Note: this middleware has to come first! Otherwise, Express will treat
+// "random" as the argument to the `id` parameter of /employees/:id.
+router.get("/random", (req, res) => {
+  const randomIndex = Math.floor(Math.random() * employees.length);
+  res.send(employees[randomIndex]);
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  // req.params are always strings, so we need to convert `id` into a number
+  // before we can use it to find the employee
+  const employee = employees.find((e) => e.id === +id);
+
+  if (!employee) {
+    return res.status(404).send("Employee not found");
+  }
+
+  res.send(employee);
+});
